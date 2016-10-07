80e776b7c179d24bac2c4e32a8f74b2de56926c3
pasca <pasca@DESKTOP-HSH8QT5>
2016-10-07 19:06:51 +0200

    initial commit QuickQuo

---
+++ QuickQuo/src/QuickQuo.java
@@ -0,0 +1,11 @@
+
+public class QuickQuo {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello QucikQuo");
+		System.out.println("");
+	}
+
+}
