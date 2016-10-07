d8571eb6992b797f594ff1826f9395e583e186c7
pasca <pasca@DESKTOP-HSH8QT5>
2016-10-07 19:14:52 +0200

    output printline test change commit

--- QuickQuo/src/QuickQuo.java
+++ QuickQuo/src/QuickQuo.java
@@ -5,7 +5,7 @@ public class QuickQuo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello QucikQuo");
-		System.out.println("");
+		System.out.println("change one, test for commiting");
 	}
 
 }
